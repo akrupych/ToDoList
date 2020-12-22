38bc956fa4b94c3bdffb49131345d199e3c74dcd
akrupych <andriy.krupych@gmail.com>
2020-12-22 13:32:53 +0200

    tasks fragment with view binding

---
+++ app/src/main/java/com/example/todolist/tasks/TasksFragment.kt
@@ -0,0 +1,20 @@
+package com.example.todolist.tasks
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.todolist.databinding.FragmentTasksBinding
+
+class TasksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
